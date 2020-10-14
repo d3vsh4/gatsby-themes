54dd8ebb400bd7aa52a28d859bbb19acc620108e
Devanshu <41241629+d3vsh4@users.noreply.github.com>
2020-10-14 09:41:39 +0530

    Update cara.tsx

--- themes/gatsby-theme-cara/src/templates/cara.tsx
+++ themes/gatsby-theme-cara/src/templates/cara.tsx
@@ -16,14 +16,14 @@ import Header from "../components/header"
 
 const Cara = () => (
   <Layout>
-    <Parallax pages={13}>
+    <Parallax pages={13.2}>
       <Header />
-      <Hero offset={0} factor={2} />
+      <Hero offset={0.1} factor={2} />
       <Projects offset={2} factor={2} />
       <About offset={4} factor={2} />
       <About2 offset={6} factor={1.2} />
-      <Photo offset={7} factor={0.5} />
-      <About3 offset={8} factor={0.5} />
+      <Photo offset={7} factor={0.6} />
+      <About3 offset={8.1} factor={0.6} />
       <Photo2 offset={9} factor={0.5} />
       <CodePhoto offset={10} factor={1} />
       {/* <Video offset={9} factor={1} /> */}
